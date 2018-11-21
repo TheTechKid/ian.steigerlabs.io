4111681bbb2c55a27a511d0801e17aaeae755635
I. Steiger <39713548+TheTechKid@users.noreply.github.com>
2018-11-21 11:08:03 -0500

    Initial Commit

---
+++ js/IanJS.js
@@ -0,0 +1,21 @@
+// Snackbar Code
+function snackbarJS() {
+	"use strict";
+    var x = document.getElementById("snackbar");
+	console.log("Thanks For Downloading!");
+    x.className = "show bg-light text-dark rounded";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
+// checks if /var/www/html/update.js = yes or no
+var x = document.getElementById("rebootalert");
+x.className = "invisible";
+if (update === "yes") {
+x.className = x.className.replace("invisible", "alert alert-warning visible"); 
+}
+// Rickroll'd
+var dateget = new Date();
+var month = dateget.getMonth();
+var day = dateget.getDate();
+if (day === 1 && month === 3) {
+ window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
---
+++ js/RickrollJS.js
@@ -0,0 +1,7 @@
+// Rickroll'd
+var dateget = new Date();
+var month = dateget.getMonth();
+var day = dateget.getDate();
+if (day === 1 && month === 3) {
+ window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
